6cee609e203c75699aca2245240a948476e1b673
Mahnegar Mahmoudi <mahnegar.mahmoudi@sekke.local>
2025-01-05 17:00:52 +0330

    fetch api and error handling in useEffect hook

---
+++ side-effects/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Users from './components/Users'
+
+function App() {
+
+  return (
+    <>
+      <Users />
+    </>
+  )
+}
+
+export default App
---
+++ side-effects/src/components/Users.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+
+function Users() {
+    const [users, setUsers] = useState([])
+    const [error,setError] = useState(false)
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const res = await fetch("https://jsonplaceholder.typicode.com/users")
+                const json = await res.json()
+                setUsers(json)
+            } catch (error) {
+              setError(true)
+
+            }
+        }
+        fetchUsers()
+    }, [])
+
+    return (
+        <div>
+            {!users.length && !error && <p>Loading</p>}
+
+            <ul>{users.map(user => (<li key={user.id} >{user.name}</li>))}</ul>
+
+            {error && <h3>Something went wrong!!!</h3>}
+        </div>
+    )
+}
+
+export default Users
