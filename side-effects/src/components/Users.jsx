2cf173cbaa6690399bbfe3577128d0845083bfb5
Mahnegar Mahmoudi <mahnegarmahmoudi@gmail.com>
2025-01-07 15:42:04 +0330

    events in http requests

--- side-effects/src/components/Users.jsx
+++ side-effects/src/components/Users.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react'
 function Users() {
     const [users, setUsers] = useState([])
     const [error, setError] = useState(false)
+    const [id, setId] = useState("")
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -19,11 +20,22 @@ function Users() {
         fetchUsers()
     }, [])
 
+
+    const searchHandler = async () => {
+        const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+        const json =await res.json()
+       console.log(json);
+       
+    }
+
+
     return (
         <div>
+            <input type='text' placeholder='enter id' value={id} onChange={(e) => setId(e.target.value)} />
+            <button onClick={searchHandler} >search</button>
             {!users.length && !error && <p>Loading</p>}
 
-            <ul>{users.map(user => (<li key={user.id} >{user.name}</li>))}</ul>
+            {/* <ul>{users.map(user => (<li key={user.id} >{user.name}</li>))}</ul> */}
 
             {error && <h3>Something went wrong!!!</h3>}
         </div>
