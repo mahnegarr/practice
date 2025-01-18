7585e21f421f355df0d14144d6a49587efba0409
Mahnegar Mahmoudi <mahnegar.mahmoudi@sekke.local>
2025-01-18 16:57:56 +0330

    action of reducers handling

---
+++ useReducer-hook/src/App.jsx
@@ -0,0 +1,47 @@
+import { useReducer, useState } from 'react'
+
+const reducer = (state, action) => {
+
+  switch (action) {
+    case "Increase":
+      return state + 1
+    case "Decrease":
+      return state - 1
+    case "Reset":
+      return state = 0
+    default:
+      break;
+  }
+
+}
+function App() {
+
+  const [count, dispatch] = useReducer(reducer, 0)
+
+
+
+  const increaseHandler = () => {
+    dispatch("Increase")
+  }
+
+  const resetHandler = () => {
+    dispatch("Reset")
+  }
+
+  const decreaseHandler = () => {
+    dispatch("Decrease")
+  }
+
+  return (
+    <>
+      <div>
+        <h1>{count}</h1>
+        <button onClick={increaseHandler}>up</button>
+        <button onClick={resetHandler}>reset</button>
+        <button onClick={decreaseHandler}>down</button>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ useReducer-hook/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import structuredClone from 'structured-clone';
+
+if (typeof global.structuredClone === 'undefined') {
+    global.structuredClone = structuredClone;
+}
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
